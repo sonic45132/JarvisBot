3afb890ce93221927e6fcb9183fc5e09d5f11731
sonic45132 <sonicblue700@yahoo.com>
2013-06-04 17:27:27 -0400

    Eve api updates

--- eve_parser.rb
+++ eve_parser.rb
@@ -13,7 +13,7 @@ class EveParser
       settings = Psych.load_file(File.dirname(__FILE__)+'/config.yaml')
       EAAL.cache = EAAL::Cache::FileCache.new
       puts 'Connecting to Eve API...'
-      @api = EAAL::API.new('2221766', 'auMDqfF7zmtDbopHjKPvtgyQSYBXytHGA8uEWVZRpiFfPja7ISpHJEf4H8FJ2TUX')
+      @api = EAAL::API.new(settings['userid'], settings['apikey'])
       puts 'Connect to Eve'
       result = @api.Characters
       result.characters.each{|character|
@@ -23,23 +23,26 @@ class EveParser
   end
 
   def setup
-    puts File.dirname(__FILE__)+'/configs/eve_config.yaml'
     if !File.exist? File.dirname(__FILE__)+'/configs/eve_config.yaml'
       settings = Hash.new()
       print 'Enter api user id: '
       settings['userid'] = gets.chomp
       print 'Enter api key: '
       settings['apikey'] = gets.chomp
-      File.open(File.dirname(__FILE__)+'/configs/eve_config','w') do |file|
+      File.open(File.dirname(__FILE__)+'/configs/eve_config.yaml','w') do |file|
         file.puts settings.to_yaml
       end
     end
   end
 
   def parse(msg)
-    charid = @api.Characters.characters.first.characterID
-    @api.scope = 'char'
-    return queue = @api.SkillQueue('characterID' => charid)
+    if msg.body.downcase == 'eve queue'
+      charid = @api.Characters.characters.first.characterID
+      @api.scope = 'char'
+      return queue = @api.SkillQueue('characterID' => charid).skillqueue
+    else
+      return nil
+    end
   end
 
 end

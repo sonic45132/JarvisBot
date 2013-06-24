dd1ea5deaace407b8c7e32fc7a9464c4536e3128
sonic45132 <sonicblue700@yahoo.com>
2013-06-24 19:01:28 -0400

    Added initial stages of an email parser and the beginning structure to the message parsing tree.

---
+++ parsers/mail_parser.rb
@@ -0,0 +1,18 @@
+require 'mail'
+require 'psych'
+
+class MailParser
+
+  def initialize(config_path)
+
+  end
+
+  def setup
+
+  end
+
+  def parse(msg)
+    return 'hi'
+  end
+
+end
---
+++ tree_node.rb
@@ -0,0 +1,10 @@
+class TreeNode
+
+  attr_reader :matchers, :children
+
+  def initialize
+    @matchers = Array.new
+    @children = Array.new
+  end
+
+end
